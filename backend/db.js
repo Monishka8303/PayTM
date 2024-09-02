1d410321fd4830f229f91f7fc9f49e046c47d07d
monishka8303 <monishka8303@gmail.com>
2024-09-02 14:03:13 +0530

    Mongo URI hidden

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost:27017/paytm').then(()=>console.log("Connected!"))
+mongoose.connect(process.env.MONGO_URI).then(()=>console.log("Connected!"))
 
 const userSchema=new mongoose.Schema({
     firstName: {
